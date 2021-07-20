01006850fe889754f57c2087b940bfcbb20a4d1e
A-Jha <avvinashjha@gmail.com>
2021-07-20 17:13:02 +0530

    new complete conceptual dive is coming

--- BasicDRF/api_basic/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Artical
-
-admin.site.register(Artical)
--- BasicDRF/api_basic/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Artical
-
-
-class ArticalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Artical
-        fields = ['id', 'title', 'author', 'email']
